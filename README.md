bb21191302663e02f57af20a3f439f40c9638de1
08183080 <08183080@cumt.edu.cn>
2024-11-24 21:57:15 +0800

    11/24，成功爬取一页

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 
 # log
 - [x] 11/23，启发于github-spider项目，准备爬取github关键词的repo数据，爬虫受限
+- [x] 11/24，基于ref3文章，请求加了token，成功爬取，但爬了两次后，频率限制不能爬了，或许增量爬虫，把数据爬下来保存再去搜索才是王道 
 
 # refs
-- [x] https://github.com/yanglinlong6/github-spider
-- [x] https://huggingface.co/spaces/Aniun/semantic_github
+- [x] 1.https://github.com/yanglinlong6/github-spider
+- [x] 2.https://huggingface.co/spaces/Aniun/semantic_github
+- [x] 3.Python脚本爬取海量Github仓库 https://blog.csdn.net/weixin_39132520/article/details/114925354
--- test/search.py
+++ test/search.py
@@ -1,13 +1,19 @@
 import requests
 
-url = 'https://github.com/search?q=推荐算法 language:Python&type=repositories&l=Python'
+url = 'https://github.com/search?q=推荐算法 language:Python&type=repositories&l=Python&s=stars&o=desc&p=1'
+
+headers = {'User-Agent':'Mozilla/5.0',
+           'Authorization': 'token ef802a122df2e4d29d9b1b868a6fefb14f22b272',
+           'Content-Type':'application/json',
+           'Accept':'application/json'
+          }
 
 proxies = {
     'http': '127.0.0.1:7890',
     'https': '127.0.0.1:7890'
 }
 
-response = requests.get(url, proxies=proxies)
+response = requests.get(url, proxies=proxies, headers=headers)
 
 # print(response.text)
 
